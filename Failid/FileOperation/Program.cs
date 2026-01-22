895e46dfc70372859bb23f10dbc0739c766ddbbe
Estlib <75094683+Estlib@users.noreply.github.com>
2026-01-22 10:04:15 +0200

    loe failist

--- Failid/FileOperation/Program.cs
+++ Failid/FileOperation/Program.cs
@@ -1,15 +1,47 @@
-﻿namespace FileOperation
+﻿
+namespace FileOperation
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kirjuta siia oma postkasti aadress");
+            //Console.WriteLine("Kirjuta siia oma postkasti aadress");
 
-            string filePath = "G:\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\FileOperation\\postkastiaadress.txt";
-            string aadress = Console.ReadLine();
+            //string filePath = "G:\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\FileOperation\\postkastiaadress.txt";
+            //string aadress = Console.ReadLine();
 
-            File.WriteAllText(filePath, aadress);
+            //File.WriteAllText(filePath, aadress);
+
+            Console.WriteLine("Kõik laulusõnad");
+
+            DisplayThisFile();
+        }
+
+        private static void DisplayThisFile()
+        {
+            string line = "";
+            try
+            {
+                using (
+                    StreamReader readThisThing = new StreamReader
+                    ("G:\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\FileOperation\\mjbj.txt") )
+                {
+                    int lineCount = 0;
+                    while (readThisThing.EndOfStream == false)
+                    {
+                        lineCount++;
+                        line = readThisThing.ReadLine();
+                        int pikkus = line.Length;
+                        Console.WriteLine(lineCount+" "+line+" . Reapikkus on"+pikkus);
+                    }
+                    readThisThing.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ei saa lugeda sest:"+e.Message);
+                throw;
+            }
         }
     }
 }
