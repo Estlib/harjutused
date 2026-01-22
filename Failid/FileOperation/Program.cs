c2fa85a459b8028e598a7f8cc855bbdda4fe4216
Estlib <75094683+Estlib@users.noreply.github.com>
2026-01-22 09:38:41 +0200

    file1

---
+++ Failid/FileOperation/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace FileOperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kirjuta siia oma postkasti aadress");
+
+            string filePath = "G:\\Users\\K\\Desktop\\thing\\harjutused\\Failid\\FileOperation\\postkastiaadress.txt";
+            string aadress = Console.ReadLine();
+
+            File.WriteAllText(filePath, aadress);
+        }
+    }
+}
